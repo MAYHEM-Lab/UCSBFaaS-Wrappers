3d50aebfdb6ed5e2d08ad2b808fff4ad00983558
ckrintz <ckrintz@yahoo.com>
2017-08-11 12:54:34 -0700

    fix cli

--- tools/timings/mr.sh
+++ tools/timings/mr.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-ACCT=$1
-PROF=$2
+PROF=$1
 MRBKT=spot-mr-bkt #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run without --no_spotwrap
 MRBKTNS=spot-mr-bkt-ns #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run with --no_spotwrap
 JOBID=job3000  #must match reducerCoordinator "job_id" in setupconfig.json when setupApps.py is run without --no_spotwrap
@@ -12,6 +11,7 @@ CWDIR=${PREFIX}/tools/cloudwatch
 TOOLSDIR=${PREFIX}/tools/timings
 MRDIR=${PREFIX}/lambda-python/mr
 SPOTTABLE=spotFns #must match tablename used by SpotWrap.py.template
+TS=1401861965497 #some early date
 
 #delete db entries
 cd ${DYNDBDIR}
@@ -22,10 +22,10 @@ deactivate
 cd ${LAMDIR}
 . ./venv/bin/activate
 cd ${CWDIR}
-python downloadLogs.py "/aws/lambda/mapper" ${ACCT} -p ${PROF} --deleteOnly
-python downloadLogs.py "/aws/lambda/reducer" ${ACCT} -p ${PROF} --deleteOnly
-python downloadLogs.py "/aws/lambda/driver" ${ACCT} -p ${PROF} --deleteOnly
-python downloadLogs.py "/aws/lambda/reducerCoordinator" ${ACCT} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/mapper" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducer" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/driver" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerCoordinator" ${TS} -p ${PROF} --deleteOnly
 deactivate
 
 for i in `seq 1 1`;
@@ -36,17 +36,17 @@ do
     cd ${MRDIR}
     . ../venv/bin/activate
     #run the driver
-    aws lambda invoke --invocation-type Event --function-name driverNS --region us-west-2 --profile ${PROF} --payload '{"eventSource":"ext:invokeCLI","prefix":"pavlo/text/1node/uservisits/","job_id":"${JOBID}","mapper":"mapper","reducer":"reducer","bucket":"big-data-benchmark","jobBucket":"${MRBKT}","region":"us-west-2","full_async":"yes"}' outputfile
+    aws lambda invoke --invocation-type Event --function-name driver --region us-west-2 --profile ${PROF} --payload '{"eventSource":"ext:invokeCLI","prefix":"pavlo/text/1node/uservisits/","job_id":"${JOBID}","mapper":"mapper","reducer":"reducer","bucket":"big-data-benchmark","jobBucket":"${MRBKT}","region":"us-west-2","full_async":"yes"}' outputfile
     sleep 15m
 done
 
 #download cloudwatch logs (and delete them)
 cd ${CWDIR}
 mkdir -p $i/MR
-python downloadLogs.py "/aws/lambda/mapper" ${ACCT} -p ${PROF} --delete  > $i/MR/map.log
-python downloadLogs.py "/aws/lambda/reducer" ${ACCT} -p ${PROF} --delete  > $i/MR/red.log
-python downloadLogs.py "/aws/lambda/driver" ${ACCT} -p ${PROF} --delete  > $i/MR/driv.log
-python downloadLogs.py "/aws/lambda/reducerCoordinator" ${ACCT} -p ${PROF} --delete  > $i/MR/coord.log
+python downloadLogs.py "/aws/lambda/mapper" ${TS} -p ${PROF} --delete  > $i/MR/map.log
+python downloadLogs.py "/aws/lambda/reducer" ${TS} -p ${PROF} --delete  > $i/MR/red.log
+python downloadLogs.py "/aws/lambda/driver" ${TS} -p ${PROF} --delete  > $i/MR/driv.log
+python downloadLogs.py "/aws/lambda/reducerCoordinator" ${TS} -p ${PROF} --delete  > $i/MR/coord.log
 deactivate
     
 #download the db and then delete its entries
