b502b5f3d4cf7e70f9cbe6637768ccd8aa5a3129
ckrintz <ckrintz@yahoo.com>
2017-08-11 13:44:48 -0700

    fixing looping issue

--- tools/timings/mr.sh
+++ tools/timings/mr.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+if [ -z ${1+x} ]; then echo 'Unset args. Set and rerun. Exiting...!'; exit 1; fi
 PROF=$1
 MRBKT=spot-mr-bkt #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run without --no_spotwrap
 MRBKTNS=spot-mr-bkt-ns #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run with --no_spotwrap
@@ -36,26 +37,27 @@ do
     cd ${MRDIR}
     . ../venv/bin/activate
     #run the driver
-    aws lambda invoke --invocation-type Event --function-name driver --region us-west-2 --profile ${PROF} --payload '{"eventSource":"ext:invokeCLI","prefix":"pavlo/text/1node/uservisits/","job_id":"${JOBID}","mapper":"mapper","reducer":"reducer","bucket":"big-data-benchmark","jobBucket":"${MRBKT}","region":"us-west-2","full_async":"yes"}' outputfile
+    aws lambda invoke --invocation-type Event --function-name driver --region us-west-2 --profile ${PROF} --payload "{\"eventSource\":\"ext:invokeCLI\",\"prefix\":\"pavlo/text/1node/uservisits/\",\"job_id\":\"${JOBID}\",\"mapper\":\"mapper\",\"reducer\":\"reducer\",\"bucket\":\"big-data-benchmark\",\"jobBucket\":\"${MRBKT}\",\"region\":\"us-west-2\",\"full_async\":\"yes\"}" outputfile
     /bin/sleep 900 #seconds, so 15mins
-done
 
-#download cloudwatch logs (and delete them)
-cd ${CWDIR}
-mkdir -p $i/MR
-python downloadLogs.py "/aws/lambda/mapper" ${TS} -p ${PROF} --delete  > $i/MR/map.log
-python downloadLogs.py "/aws/lambda/reducer" ${TS} -p ${PROF} --delete  > $i/MR/red.log
-python downloadLogs.py "/aws/lambda/driver" ${TS} -p ${PROF} --delete  > $i/MR/driv.log
-python downloadLogs.py "/aws/lambda/reducerCoordinator" ${TS} -p ${PROF} --delete  > $i/MR/coord.log
-deactivate
+    #download cloudwatch logs (and delete them)
+    cd ${CWDIR}
+    mkdir -p $i/MR
+    python downloadLogs.py "/aws/lambda/mapper" ${TS} -p ${PROF} --delete  > $i/MR/map.log
+    python downloadLogs.py "/aws/lambda/reducer" ${TS} -p ${PROF} --delete  > $i/MR/red.log
+    python downloadLogs.py "/aws/lambda/driver" ${TS} -p ${PROF} --delete  > $i/MR/driv.log
+    python downloadLogs.py "/aws/lambda/reducerCoordinator" ${TS} -p ${PROF} --delete  > $i/MR/coord.log
+    deactivate
     
-#download the db and then delete its entries
-cd ${DYNDBDIR}
-. ./venv/bin/activate
-rm -rf dump
-python dynamodump.py -m backup -r us-west-2 -p ${PROF} -s ${SPOTTABLE}
-mkdir -p $i/MR
-mv dump $i/MR/
-python dynamodelete.py -p ${PROF} ${SPOTTABLE}
-deactivate
+    #download the db and then delete its entries
+    cd ${DYNDBDIR}
+    . ./venv/bin/activate
+    rm -rf dump
+    python dynamodump.py -m backup -r us-west-2 -p ${PROF} -s ${SPOTTABLE}
+    mkdir -p $i/MR
+    rm -rf $i/MR/*
+    mv dump $i/MR/
+    python dynamodelete.py -p ${PROF} ${SPOTTABLE}
+    deactivate
+done
 
