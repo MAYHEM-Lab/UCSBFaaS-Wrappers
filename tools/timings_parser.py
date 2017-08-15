8c7b837171dbec59ca6db85ceed027734cfa8508
ckrintz <ckrintz@yahoo.com>
2017-08-15 13:57:18 -0700

    refactoring

--- tools/timings_parser.py
+++ tools/timings_parser.py
@@ -31,36 +31,64 @@ def check_dir(dir_key,event,job):
                 retn = False
     return retn
         
-def processCW(dirname,NSJob,ofile,skipFirst=False):
+def processCW(dirname,jobcount,NSJob,ofile,skipFirst=False):
+    return
+    fnames = [] #coord.log, driv.log, map.log, red.log
+    ns_str = '/NS' if NSJob else ''
+    for n in range(2,jobcount):
+        if not skipFirst or n > 1:
+            fnames.append('{}/{}{}/'.format(dirname,n,ns_str)) #dirnames
+    dsize = 0.0
+    keycount = 0
+    keys = 0 #unused
+    lines = 0 #unused
+    timer = 0.0
+    err = None
+    ids = 0
+    tlist = []
+    for fname in fnames:
+        if not os.path.exists(fname):
+            print('processCW: Error: {} not found!'.format(fname))
+            sys.exit(1)
+        ids += 1
+        with open(fname,'r') as f:
+            for line in f:
+                line = line.strip()
+                if DEBUG:
+                    print('{}:{}'.format(fname,line))
+                if line.startswith('Dataset size'):
+                    sline = line.split(' ')
+                    dsize = float(sline[2].strip(','))
+                    keycount = int(sline[4].strip(','))
+                if line.startswith('Num. of Mappers'):
+                    sline = line.split(' ')
+                    mappers = int(sline[6])
+                if line.startswith('mapper output'):
+                    sline = line.split(' ')
+                    if NSJob:
+                        keys = int(sline[2].strip('[,'))
+                        lines = int(sline[3].strip(','))
+                        timer = float(sline[4].strip(','))
+                        err = sline[5].strip("']")
+                    else:
+                        keys = int(sline[5].strip('[,'))
+                        lines = int(sline[6].strip(','))
+                        timer = float(sline[7].strip(','))
+                        err = sline[8].strip("']}")
+                    if err == '': 
+                        err = None
+                    if not err:
+                        tlist.append(timer)
+    return (ids,statistics.mean(tlist),statistics.stdev(tlist),mappers,dsize,keycount)
+
+def processDB(dirname,jobcount,NSJob,ofile,skipFirst=False):
     pass
-def processDB(dirname,NSJob,ofile,skipFirst=False):
-    pass
-def processMR(dirname,NSJob,ofile,skipFirst=False):
+def processMR(dirname,jobcount,NSJob,ofile,skipFirst=False):
     fnames = []
-    if NSJob:
-        if not skipFirst:
-            fnames.append('{}/1/NS/overhead.out'.format(dirname))
-        fnames.append('{}/2/NS/overhead.out'.format(dirname))
-        fnames.append('{}/3/NS/overhead.out'.format(dirname))
-        fnames.append('{}/4/NS/overhead.out'.format(dirname))
-        fnames.append('{}/5/NS/overhead.out'.format(dirname))
-        fnames.append('{}/6/NS/overhead.out'.format(dirname))
-        fnames.append('{}/7/NS/overhead.out'.format(dirname))
-        fnames.append('{}/8/NS/overhead.out'.format(dirname))
-        fnames.append('{}/9/NS/overhead.out'.format(dirname))
-        fnames.append('{}/10/NS/overhead.out'.format(dirname))
-    else:
-        if not skipFirst:
-            fnames.append('{}/1/overhead.out'.format(dirname))
-        fnames.append('{}/2/overhead.out'.format(dirname))
-        fnames.append('{}/3/overhead.out'.format(dirname))
-        fnames.append('{}/4/overhead.out'.format(dirname))
-        fnames.append('{}/5/overhead.out'.format(dirname))
-        fnames.append('{}/6/overhead.out'.format(dirname))
-        fnames.append('{}/7/overhead.out'.format(dirname))
-        fnames.append('{}/8/overhead.out'.format(dirname))
-        fnames.append('{}/9/overhead.out'.format(dirname))
-        fnames.append('{}/10/overhead.out'.format(dirname))
+    ns_str = '/NS' if NSJob else ''
+    for n in range(2,jobcount):
+        if not skipFirst or n > 1:
+            fnames.append('{}/{}{}/overhead.out'.format(dirname,n,ns_str)) #file names
     dsize = 0.0
     keycount = 0
     keys = 0 #unused
@@ -119,14 +147,14 @@ def parseIt(event,job='both',skipFirst=False):
     print("JOB: {}".format(job))
     with open(event['output_fname'],'w') as ofile: #first to open so overwrite and start fresh
         if job=='NS' or job=='BOTH':
-            count,avg,stdev,mcount,dsize,keys = processMR(event['mrdir'],True,ofile,skipFirst)
+            count,avg,stdev,mcount,dsize,keys = processMR(event['mrdir'],event['count'],True,ofile,skipFirst)
             print('NStotal:{},map_avg:{}:map_stdev:{}:map_count:{}:dsize:{}:keys:{}'.format(count,avg,stdev,mcount,dsize,keys))
         if job=='SPOT' or job=='BOTH':
-            count,avg,stdev,mcount,dsize,keys = processMR(event['mrdir'],False,ofile,skipFirst)
+            count,avg,stdev,mcount,dsize,keys = processMR(event['mrdir'],event['count'],False,ofile,skipFirst)
             print('SPOTtotal:{},map_avg:{}:map_stdev:{}:map_count:{}:dsize:{}:keys:{}'.format(count,avg,stdev,mcount,dsize,keys))
-        if not processCW(event['cwdir'],job,ofile):
+        if not processCW(event['cwdir'],event['count'],job,ofile):
             print('processCW failed, exiting...')
-        if not processDB(event['dbdir'],job,ofile):
+        if not processDB(event['dbdir'],event['count'],job,ofile):
             print('processDB failed, exiting...')
 
 if __name__ == "__main__":
@@ -138,6 +166,7 @@ if __name__ == "__main__":
     parser.add_argument('--process_NS_only',action='store_true',default=False,help='process the NS subdirectories (a non-SpotWrap job)')
     parser.add_argument('--process_spot_only',action='store_true',default=False,help='process the NS subdirectories (a non-SpotWrap job)')
     parser.add_argument('--skip_first',action='store_true',default=False,help='skip the first job (warmup)')
+    parser.add_argument('--count',action='store',type=int,default=10,help='number of job dirs to process (default = 10)')
     args = parser.parse_args()
     if args.process_NS_only and args.process_spot_only:
         print('Error, process_NS_only and process_spot_only cannot both be set, use one or none and rerun')
@@ -147,6 +176,7 @@ if __name__ == "__main__":
     event['cwdir'] = args.cwdir
     event['dbdir'] = args.dbdir
     event['output_fname'] = args.output_file
+    event['count'] = args.count
     run = 'BOTH'
     if args.process_spot_only:
         run = 'SPOT'
