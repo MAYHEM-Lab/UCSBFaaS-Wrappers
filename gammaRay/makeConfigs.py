f16e905248428c968aca99842eb656bf9588b253
ckrintz <ckrintz@yahoo.com>
2017-09-03 10:56:36 -0700

    fixing minor bug in exception handling for deleteAll option

--- gammaRay/makeConfigs.py
+++ gammaRay/makeConfigs.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='make python3 configs for setupApps.py')
     parser.add_argument('configDir',action='store',default='configs',help='directory into which the config files will go')
     parser.add_argument('--swbkt',action='store',default='cjktestbkt',help='bucket for spotwrap libs')
+    parser.add_argument('--swbkteast',action='store',default='cjktestbkteast',help='bucket for spotwrap libs')
     args = parser.parse_args()
     
-    suffixes = ['C', #clean/nothing default Lambda
+    suffixes = [
+        'C', #clean/nothing default Lambda
         'T', #tracing turned on
         'F', #tracing + fleece turned on
         'S', #static GammaRay (original spotwrap)
-        'D', #dynamid GammaRay (spotwrap reimplemented using fleece)
+        'D', #dynamic GammaRay (spotwrap reimplemented using fleece)
     ]
     apps = {
         'DBSyncPy':'apps/DBSync.py',
@@ -28,59 +30,107 @@ if __name__ == "__main__":
         'reducer':'apps/map-reduce/reducer.py',
         'driver':'apps/map-reduce/driver.py',
         'reducerCoordinator':'apps/map-reduce/reducerCoordinator.py',
+        'UpdateWebsite':'apps/FnInvoker.py',
+    }
+    #specify the bucket and prefix (folder) in bucket to use to trigger function lambda_name
+    #bucket names must be unique and identifiable (for run type: C,T,F,S,D)
+    triggerBuckets = {  #lambda_name:bucket, bucket_prefix:prefix
+        ('ImageProcPyC':'image-proc-c','prefix':'imageProc'),
+        ('ImageProcPyT':'image-proc-t','prefix':'imageProc'),
+        ('ImageProcPyF':'image-proc-f','prefix':'imageProc'),
+        ('ImageProcPyS':'image-proc-s','prefix':'imageProc'),
+        ('ImageProcPyD':'image-proc-d','prefix':'imageProc'),
+        ('reducerCoordinatorC':'spot-mr-bkt-ns','prefix':'job8000'),
+        ('reducerCoordinatorT':'spot-mr-bkt-t','prefix':'job8000'),
+        ('reducerCoordinatorF':'spot-mr-bkt-t2','prefix':'job8000'),
+        ('reducerCoordinatorS':'spot-mr-bkt','prefix':'job8000'),
+        ('reducerCoordinatorD':'spot-mr-bkt-gr','prefix':'job8000')
+    }
+
+    #specify the DynamoDB table stream that triggers lambda function lambda_name
+    triggerTables = { #lambda_name,table_name
+        'DBSyncPy':'imageLabels',
+        'UpdateWebsite':'eastSyncTable',
     }
     if not os.path.isdir(args.configDir):
         print('Error, filename passed in must be an existing directory (make sure its empty)')
         sys.exit(1)
 
     #now create a config for all of the lambda's in the west region
-    reg = 'us-west-2'
-    swbkt = args.swbkt
-    for suffix in suffixes:
-        fname = '{}/config{}.json'.format(args.configDir,suffix)
-        jsonf = {}
-        fun = []
-        for key in apps:
-            entry = apps[key]    
-            if not os.path.isfile(entry):
-                print('Error, script in apps list does not exist: {}'.format(entry))
-                sys.exit(1)
-            k = entry.rfind("/")
-            idx = entry.find(".py")
-            assert idx > 0
-            pyfile = entry[k+1:idx]
-            files_and_dirs = [
-                '{}'.format(entry)
-            ]
-            if 'driver' in key or 'reducerCoordinator' in key:
-                files_and_dirs.append("apps/map-reduce/lambdautils.py")
-            if suffix == 'F' or suffix == 'G':
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/fleece")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/requests")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/urllib3")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/chardet")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/certifi")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/idna")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/structlog")
-                files_and_dirs.append("apps/venv/lib/python3.6/site-packages/wrapt")
-            
-            mem = 128
-            if 'mapper' in key or 'reducer' in key:
-                mem = 1536
-            app = {
-                "name": "{}{}".format(key,suffix),
-                "lambdaMemory": mem,
-                "handler": "{}.handler".format(pyfile),
-                "zip": "package.zip",
-                "files_and_dirs": files_and_dirs,
-                "patched_botocore_dir": "venv/lib/python3.6/site-packages/botocore",
-                "s3bucket": swbkt
-            }
-            fun.append(app)
-        jsonf = {
-            "region":reg,
-            "functions":fun
-        }
-        with open(fname, 'w') as f :
-            json.dump(jsonf, f, indent=4)
+    regs = [('us-west-2','',args.swbkt),('us-east-1','East',args.swbkteast)]
+    for regtuple in regs:
+        reg = regtuple[0]
+        end = regtuple[1]
+        swbkt = regtuple[2]
+        for suffix in suffixes:
+            fname = '{}/config{}{}.json'.format(args.configDir,end,suffix)
+            jsonf = {}
+            fun = []
+            for key in apps:
+                entry = apps[key]    
+
+                #add app names two these two entries for lambdas you want to create in east only (UpdateWebsite)
+                if reg == 'us-east-1' and (key != 'UpdateWebsite'):
+                    continue
+                if reg == 'us-west-2' and (key == 'UpdateWebsite'):
+                    continue
+
+                if not os.path.isfile(entry):
+                    print('Error, script in apps list does not exist: {}'.format(entry))
+                    sys.exit(1)
+                k = entry.rfind("/")
+                idx = entry.find(".py")
+                assert idx > 0
+                pyfile = entry[k+1:idx]
+                files_and_dirs = [
+                    '{}'.format(entry)
+                ]
+                if key == 'driver' or key == 'reducerCoordinator':
+                    files_and_dirs.append("apps/map-reduce/lambdautils.py")
+                if suffix == 'F' or suffix == 'G':
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/fleece")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/requests")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/urllib3")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/chardet")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/certifi")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/idna")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/structlog")
+                    files_and_dirs.append("apps/venv/lib/python3.6/site-packages/wrapt")
+                
+                mem = 128
+                if 'mapper' in key or 'reducer' in key:
+                    mem = 1536
+                name = "{}{}".format(key,suffix)
+                app = {
+                    "name": name,
+                    "lambdaMemory": mem,
+                    "handler": "{}.handler".format(pyfile),
+                    "zip": "package.zip",
+                    "files_and_dirs": files_and_dirs,
+                    "patched_botocore_dir": "venv/lib/python3.6/site-packages/botocore",
+                    "s3bucket": swbkt
+                }
+                for needsBucket in triggerBuckets:
+                    k, v = needsBucket.items()[0]
+                    prefix = needsBucket[1]['prefix']
+                    if name == k: 
+                        app["permission"]= v
+                        app["job_id"]= prefix
+
+                for needsTable in triggerTables:
+                    k, v = needsTable.items()[0]
+                    if name == k: 
+                        app["table"]= v
+                        
+                fun.append(app)
+
+            #write out the file in json, if any functions were added to fun
+            if fun != []:
+                jsonf = {
+                    "region":reg,
+                    "functions":fun
+                }
+                with open(fname, 'w') as f :
+                    json.dump(jsonf, f, indent=4)
+
          
---
+++ tools/timings/cleanupLambdas.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#this program deletes all lambdas listed the config files under gammaRay/config/*.json and in restConfigsWest.json and restConfigsEast.json in the local dir, if present
+
+#usage: ./cleanupLambda aws_profile arn:aws:iam::ACCT:role/LAMDAROLE
+if [ -z ${1+x} ]; then echo 'Unset aws_profile (first arg). Set and rerun. Exiting...!'; exit 1; fi
+if [ -z ${2+x} ]; then echo 'Unset aws_role (second arg). Set and rerun. Exiting...!'; exit 1; fi
+export AWSRole=$2
+PROF=$1
+PREFIX=/Users/ckrintz/RESEARCH/lambda/UCSBFaaS-Wrappers
+GRMDIR=${PREFIX}/gammaRay
+DYNDBDIR=${PREFIX}/tools/dynamodb
+CWDIR=${PREFIX}/tools/cloudwatch
+TOOLSDIR=${PREFIX}/tools/timings
+MRDIR=${PREFIX}/lambda-python/mr
+
+cd ${GRMDIR}
+. venv/bin/activate
+
+for i in `ls configs/*.json`; 
+do
+    python setupApps.py -p ${PROF} --deleteAll -f $i
+done
+if [ -e restConfigsWest.json ]
+then
+python setupApps.py -p ${PROF} --deleteAll -f restConfigsWest.json
+fi
+if [ -e restConfigsEast.json ]
+then
+python setupApps.py -p ${PROF} --deleteAll -f restConfigsEast.json
+fi
+
+deactivate
