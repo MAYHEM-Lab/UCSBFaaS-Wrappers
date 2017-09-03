e05393bf3dd3bb4d7b90f3e47312c3e08537caf1
ckrintz <ckrintz@yahoo.com>
2017-09-03 08:31:41 -0700

    adding new lambda names

--- tools/timings/cleanupAWS.sh
+++ tools/timings/cleanupAWS.sh
@@ -6,6 +6,7 @@ MRBKTNS=spot-mr-bkt-ns #must match reducerCoordinator "permission" in setupconfi
 MRBKTGR=spot-mr-bkt-gr #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run with --no_spotwrap
 MRBKTF=spot-mr-bkt-f #must match reducerCoordinator "permission" in setupconfig.json when setupApps.py is run with --no_spotwrap
 PREFIX=/Users/ckrintz/RESEARCH/lambda/UCSBFaaS-Wrappers
+GRDIR=${PREFIX}/gammaRay
 LAMDIR=${PREFIX}/lambda-python
 DYNDBDIR=${PREFIX}/tools/dynamodb
 CWDIR=${PREFIX}/tools/cloudwatch
@@ -21,17 +22,17 @@ aws s3 rm s3://${MRBKT}/ --recursive --profile ${PROF}
 aws s3 rm s3://${MRBKTNS}/ --recursive --profile ${PROF}
 aws s3 rm s3://${MRBKTGR}/ --recursive --profile ${PROF}
 aws s3 rm s3://${MRBKTF}/ --recursive --profile ${PROF}
+
 #delete dynamodb entries in table ${SPOTTABLE}
-cd ${LAMDIR}
+cd ${GRDIR}
 . venv/bin/activate
 cd ${DYNDBDIR}
 python dynamodelete.py -p ${PROF} ${SPOTTABLE}
 python dynamodelete.py -p ${PROF} ${GAMMATABLE}
-deactivate
-#delete the logs for the lambdas with spotwrap and without (NS)
-cd ${MRDIR}
-. ../venv/bin/activate
+
+#delete the logs for the lambdas
 cd ${CWDIR}
+#old names - it won't hurt anything if they aren't there
 python downloadLogs.py "/aws/lambda/mapper" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/reducer" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/driver" ${TS} -p ${PROF} --deleteOnly
@@ -52,6 +53,7 @@ python downloadLogs.py "/aws/lambda/SNSPy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/S3ModPy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/DBModPy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/DBSync" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBSyncPy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/FnInvokerPy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/SpotTemplatePy" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/SNSPyNS" ${TS} -p ${PROF} --deleteOnly
@@ -64,6 +66,54 @@ python downloadLogs.py "/aws/lambda/ImageProcPyNDB" ${TS} -p ${PROF} --deleteOnl
 python downloadLogs.py "/aws/lambda/ImageProc_F_Py" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/ImageProc_NGR_Py" ${TS} -p ${PROF} --deleteOnly
 python downloadLogs.py "/aws/lambda/ImageProc_S_Py" ${TS} -p ${PROF} --deleteOnly
+
+#new names
+python downloadLogs.py "/aws/lambda/mapperF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/driverF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerCoordinatorF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/SNSPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/S3ModPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBModPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBSyncPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/FnInvokerPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/ImageProcPyF" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/mapperT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/driverT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerCoordinatorT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/SNSPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/S3ModPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBModPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBSyncPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/FnInvokerPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/ImageProcPyT" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/mapperS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/driverS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerCoordinatorS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/SNSPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/S3ModPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBModPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBSyncPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/FnInvokerPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/ImageProcPyS" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/mapperD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/driverD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/reducerCoordinatorD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/SNSPyD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/S3ModPyD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBModPyD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/DBSyncPyD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/FnInvokerPyD" ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/ImageProcPyD" ${TS} -p ${PROF} --deleteOnly
+#now delete the ones in the east
+python downloadLogs.py "/aws/lambda/UpdateWebsiteC" --region us-east-1 ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/UpdateWebsiteT" --region us-east-1 ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/UpdateWebsiteF" --region us-east-1 ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/UpdateWebsiteS" --region us-east-1 ${TS} -p ${PROF} --deleteOnly
+python downloadLogs.py "/aws/lambda/UpdateWebsiteD" --region us-east-1 ${TS} -p ${PROF} --deleteOnly
 deactivate
 
 cd ${TOOLSDIR}
