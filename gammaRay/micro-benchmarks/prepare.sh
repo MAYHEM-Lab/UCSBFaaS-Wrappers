e00869e9b248ec77308cd08e58e5cc1695e29885
Chandra Krintz <ckrintz@yahoo.com>
2017-09-05 12:37:12 -0700

    cleanup

--- gammaRay/micro-benchmarks/prepare.sh
+++ gammaRay/micro-benchmarks/prepare.sh
@@ -1,5 +1,7 @@
-S3BKT=cjk-apitest-bucket
-DYNAMODB=noTriggerTestTable
+if [ -z ${1+x} ]; then echo 'Unset S3BKT (arg1). Set and rerun. Exiting.  ..!'; exit 1; fi
+if [ -z ${2+x} ]; then echo 'Unset DYNAMODB tablename (arg2). Set and rerun. Exiting.  ..!'; exit 1; fi
+S3BKT=$1
+DYNAMODB=$2
 touch file
 for i in `seq 101 1000`;
 do
