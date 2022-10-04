58dbd65fa04974beaa3f9e59986fc90f31dd435c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:57:43 +0000

    Adding numeric_owner as keyword arguement

--- tools/dynamodb/dynamodump.py
+++ tools/dynamodb/dynamodump.py
@@ -211,7 +211,7 @@ def do_get_s3_archive(profile, region, bucket, table, archive):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(a, path=".")
